b74ffbd2341cdfa333676475d900f14a1d6311eb
Chessnut <chessnutist@gmail.com>
2013-12-05 16:06:26 -0800

    Fixed equip bug with m9kweapons.

--- plugins/m9k/items/weapon/sh_m9k_m92.lua
+++ plugins/m9k/items/weapon/sh_m9k_m92.lua
@@ -1,6 +1,6 @@
 ITEM.name = "M92 Beretta"
 ITEM.desc = "A semi-automatic pistol designed and manufactured in Italy."
 ITEM.model = Model("models/weapons/w_beretta_m92.mdl")
-ITEM.class = ITEM.uniqueID
+ITEM.class = string.gsub(ITEM.uniqueID, "weapon_", "")
 ITEM.type = "pistol"
 ITEM.price = 800
